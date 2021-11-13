051f81b6cbff8e568c56f9a128264818592300a2
EnriquePernia <Epernia96@gmail.com>
2021-11-13 14:37:39 +0100

    Added docker examples dir

---
+++ docker/simple-container.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Pulls the image
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+# Create a nginx container
+resource "docker_container" "foo" {
+  image = docker_image.nginx.latest
+  name  = "tf-test"
+
+  # Expose 80 internal port on 8080 external
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
