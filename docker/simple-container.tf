fdf39d84201d601b20076d4b2f40e1d067e3b129
EnriquePernia <Epernia96@gmail.com>
2021-11-15 12:28:18 +0100

    Added random_id resource for naming

--- docker/simple-container.tf
+++ docker/simple-container.tf
@@ -11,6 +11,12 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# Provides a random name for resources
+resource "random_id" "rnd_container_name" {
+  prefix = "container-"
+  byte_length = 8
+}
+
 # Pulls the image
 resource "docker_image" "nginx" {
   name = "nginx:latest"
@@ -19,7 +25,7 @@ resource "docker_image" "nginx" {
 # Create a nginx container
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "nginx"
+  name  = random_id.rnd_container_name.hex
 
   # Expose 80 internal port on 8080 external
   ports {
