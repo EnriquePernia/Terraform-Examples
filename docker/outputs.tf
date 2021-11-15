ee29f831876019874e118016046fef51cd425eb2
EnriquePernia <Epernia96@gmail.com>
2021-11-15 17:04:02 +0100

    Rename simple-container to main

--- docker/outputs.tf
+++ docker/outputs.tf
@@ -1,7 +1,7 @@
 output "Address" {
   # Runs a for loop to show multiple outputs. Join wont join a tuple (join(":", ["localhost", docker_container.nginx[*].ports[0].external]))
   # i.ports[*].external is outside the list because it is already a list with one element
-  # Another way without splat operator [*]
+  # Another way without splat operator [*]:
   # value = [for i in docker_container.nginx[*]: join(":", ["localhost", i.ports[0].external])]
   value = [for i in docker_container.nginx[*]: join(":", ["localhost"], i.ports[*].external)]
   description = "Local address of the Nginx docker container"
