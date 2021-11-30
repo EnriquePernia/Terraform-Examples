cf325cb139758c849efd0be8d89329be50b6ab0e
EnriquePernia <Epernia96@gmail.com>
2021-11-30 10:42:39 +0100

    for each

--- modules/container/main.tf
+++ modules/container/main.tf
@@ -1,10 +1,7 @@
 # Create a container
 resource "docker_container" "container" {
-  # Reference to the module output
   image = var.image_in
-  # Assign a random name for each container
   name = var.name_in
-  # Expose 80 internal port on 8080 external
   ports {
     internal = var.int_port_in
     # Auto allocate, if we have multiple resources we can output its value and let tf handle it
@@ -12,7 +9,6 @@ resource "docker_container" "container" {
   }
   volumes {
     container_path = var.container_path_in
-    # Path.cwd refers to ./
     host_path = var.host_path_in
   }
 }
