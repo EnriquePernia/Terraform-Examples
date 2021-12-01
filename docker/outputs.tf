cc8512505152130838bde77e34917bc2bbb3981f
EnriquePernia <Epernia96@gmail.com>
2021-12-01 11:28:40 +0100

    Cleanup

--- docker/outputs.tf
+++ docker/outputs.tf
@@ -1,21 +1,3 @@
-# output "Address" {
-#   value       = module.container[*].address
-#   description = "Local address of the Nginx docker container"
-#   sensitive   = false
-# }
-
-# output "Container_image" {
-#   # Can also be done with join:
-#   # [for i in module.container[*] : join(" - ", [i.name], [module.image.image_name], [terraform.workspace])]
-#   value       = [for i in module.container[*] : "${i.name} - ${module.image["nginx"].image_name} - ${terraform.workspace}"]
-#   description = "Name of Nginx docker container"
-# }
-
-# output "Container_internal_port" {
-#   value       = module.container[*].container_internal_port
-#   description = "Internal port of docker containers"
-# }
-
 output "Address" {
   value       = module.container[*]
   description = "Address of the containers"
--- modules/container/outputs.tf
+++ modules/container/outputs.tf
@@ -1,23 +1,3 @@
-# output "address" {
-#   # Runs a for loop to show multiple outputs. Join wont join a tuple (join(":", ["localhost", docker_container.nginx[*].ports[0].external]))
-#   # i.ports[*].external is outside the list because it is already a list with one element
-#   # Another way without splat operator [*]:
-#   # value = [for i in docker_container.nginx[*]: join(":", ["localhost", i.ports[0].external])]
-#   value       = join(" -> ", [docker_container.container.name], [join(":", ["localhost"], docker_container.container.ports[*].external)])
-#   description = "Local address of the Nginx docker container"
-#   sensitive   = false
-# }
-
-# output "container_internal_port" {
-#   value       = join(" -> ", ["Internal port"], docker_container.container.ports[*].internal)
-#   description = "Internal port of docker containers"
-# }
-
-# output "name" {
-#   value       = docker_container.container.name
-#   description = "Internal port of docker containers"
-# }
-
 output "container_info" {
   value       = { for x in docker_container.container[*] : x.name => join(":", [x.ip_address], x.ports[*]["external"]) }
   description = "Container name plus ip address and external port"
